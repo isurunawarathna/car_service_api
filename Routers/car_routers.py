2a2b9d47eb07c0e40a98a196dcd7fcd89995b739
IsuruNawarathna <isurunawarathna2006@gmail.com>
2026-05-23 15:56:49 +0530

    Create two new functions called get_all and get_by_id

--- Routers/car_routers.py
+++ Routers/car_routers.py
@@ -1,8 +1,7 @@
-
-
 from fastapi import APIRouter, status, HTTPException
 from Models.car_models import CarResponse, CarCreate
 from Repository.car_repository import InMemoryRepository, CarServiceError
+from typing import List
 
 from Service.car_service import CarService
 
@@ -15,4 +14,20 @@ def create_car(car : CarCreate) -> CarResponse:
         return car_service.create_car(car)
     except CarServiceError as e:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(e))
+
+@router.get("/id/{car_id}", response_model=CarResponse, status_code=status.HTTP_302_FOUND)
+def get_by_id(car_id : int):
+    try:
+        return car_service.get_by_id(car_id)
+    except CarServiceError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=str(e))
+
+@router.get("/all_cars",response_model=List[CarResponse], status_code=status.HTTP_302_FOUND)
+def get_all()->List[CarResponse]:
+    try:
+        return car_service.get_all()
+    except CarServiceError as e:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+
+
 
